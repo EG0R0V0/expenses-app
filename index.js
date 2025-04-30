a0823b019ecf53d93681e6f3148ebc78540be744
Egorov Roman <hihihi.ru1@mail.ru>
2025-04-30 21:54:21 +0300

    add history block and push expenseslist

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const expenses = [];
 
-const pushExpense = document.querySelector('.js-input__btn');
-const inputExpense = document.querySelector('.js-input__expens')
+const pushExpenseNode = document.querySelector('.js-input__btn');
+const inputExpenseNode = document.querySelector('.js-input__expens');
+const historyNode = document.querySelector('.js-history');
 
+pushExpenseNode.addEventListener('click', function () {
+    //1. проверяем наличие значения в поле ввода
+    if (inputExpenseNode.value === '') { // или !inputExpenseNode.value
+        return;
+    }
+
+    //2. делаем строку числом
+    const expense = parseInt(inputExpenseNode.value)
+
+    //3. после нажатия обнуляем поля ввода
+    inputExpenseNode.value = '';
+
+    //4. добавляем значение в массив
+    expenses.push(expense);
+
+    let expensesListHTML = '';
+
+    expenses.forEach(element => {
+        const elementHTML = `<li class = 'historyElement'>${element}</li>`;
+        expensesListHTML += elementHTML;
+    });
+
+    historyNode.innerHTML = `<ol class='expenesHistoryList'>${expensesListHTML}</ol>`;
+})
